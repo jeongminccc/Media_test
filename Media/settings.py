9d642556a4d098f14a5197bbf7caa8d1e875e0a8
jeongminccc <50401293+jeongminccc@users.noreply.github.com>
2019-07-31 20:53:47 +0900

    up

--- Media/settings.py
+++ Media/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '52!i1_zx4j9ipehxa15rlm+fsba$(b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
